a82cad48791c8e7300e0ff329c7fb64680f949ba
Tech-A29 <Tech-A29@Tech-A29s-MacBook-Air.local>
2017-11-23 11:54:25 +0000

    static route added

--- config/routes.rb
+++ config/routes.rb
@@ -3,5 +3,6 @@ Rails.application.routes.draw do
 
   resources :posts
   root to: "public#all"
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
